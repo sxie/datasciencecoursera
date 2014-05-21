9464729c2d6273bd3e95c02cd2cccb2f7679c0d2
sxie <sxie@users.noreply.github.com>
2014-05-21 12:59:04 -0400

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -8,17 +8,17 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
         ## to be used
         ## Return the mean of the pollutant across all monitors list
         ## in the 'id' vector (ignoring NA values)
-  col_SUMS = 0
-  col_CNTS = 0
-  for (i in id){
-    padded_id <- sprintf("%03d.csv",i)
-    dat <- read.csv(paste(getwd(),"/",directory,"/",padded_id,sep=""))
-    col_sum <- colSums(dat[pollutant], na.rm = TRUE)
-    col_cnt <- sum(!is.na(dat[pollutant]))
-    col_SUMS <- col_SUMS + col_sum
-    col_CNTS <- col_CNTS + col_cnt
-  }
-  #return mean
-  calcMean <- col_SUMS/col_CNTS
-  round(calcMean[[pollutant]], digits = 3)
+        col_SUMS = 0
+        col_CNTS = 0
+        for (i in id){
+          padded_id <- sprintf("%03d.csv",i)
+          dat <- read.csv(paste(getwd(),"/",directory,"/",padded_id,sep=""))
+          col_sum <- colSums(dat[pollutant], na.rm = TRUE)
+          col_cnt <- sum(!is.na(dat[pollutant]))
+          col_SUMS <- col_SUMS + col_sum
+          col_CNTS <- col_CNTS + col_cnt
+        }
+        #return mean
+        calcMean <- col_SUMS/col_CNTS
+        round(calcMean[[pollutant]], digits = 3)
 }
